be8996550c9dca6e583fd3efdb7b3b957e3eaf50
Gustavo007-del <shijilkt27@gmail.com>
2025-03-17 17:01:34 +0530

    5th commit

--- buble.cpp
+++ buble.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 void buble(int arr[], int n){
     for(int i=0;i<n-1;i++){
+        int swapis=0;
         for(int j=i;j<n-1;j++){
-            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+            if(arr[j]>arr[j+1]) {
+                swap(arr[j],arr[j+1]); 
+                swapis=1;
         }
+        if(swapis==0) break;
         for(int i=0;i<n;i++){
             cout<<arr[i]<<" ";}
             cout<<endl;
     }
 
+}
 }
 int main(){
     int n;
