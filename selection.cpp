68baa460932bf3d9d5fa02c6bb3f4ea43fe72732
Gustavo007-del <shijilkt27@gmail.com>
2025-03-17 15:48:33 +0530

    third commit

--- selection.cpp
+++ selection.cpp
@@ -3,7 +3,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int main(){
-        
-        
+void selsort(int arr[], int n){
+    for(int i=0;i<=n-2;i++){
+        int min=i;
+            for(int j=i;j<=n-1;j++){
+                if(arr[j]<arr[min]) min=j;
+            }
+        swap(arr[min],arr[i]);
     }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    selsort(arr , n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+        }
+}
